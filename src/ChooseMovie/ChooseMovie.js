57398f3fafe07eefea0af2e726a9873947038243
farias_77 <gabrielfalves2003@gmail.com>
2022-05-17 14:53:52 -0300

    Página escolher filme

--- src/ChooseMovie/ChooseMovie.js
+++ src/ChooseMovie/ChooseMovie.js
@@ -1,10 +1,14 @@
 import "./ChooseMovie.css";
 
+import Movie from "../Movie/Movie.js";
+
 export default function ChooseMovie(){
     return (
         <div className="chooseMovie">
             <div className="select">Selecione o filme</div>
-            <div className="movies"></div>
+            <div className="movies">
+                <Movie />
+            </div>
         </div>
     )
 }
---
+++ src/Movie/Movie.js
@@ -0,0 +1,9 @@
+import "./Movie.css";
+
+export default function Movie(){
+    return (
+        <div className="movie">
+            <img src="https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg" alt="movie banner" />
+        </div>
+    )
+}
