7d3b0f352432ab17be338fcf3891f7549e2f89fb
farias_77 <gabrielfalves2003@gmail.com>
2022-05-21 11:47:40 -0300

    Versão final cineflex

--- src/Success/Success.js
+++ src/Success/Success.js
@@ -31,7 +31,6 @@ export default function Success({name, cpf, selectedSeats, sessionInfo, selected
         setSelectedSeats([]);
         setSelectedSeatsId([]);
         setSessionInfo("");
-        console.log("siiiu");
     }
 
     return (
